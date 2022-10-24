195b7f6d9a79893c830bb772ba331aa63441ab47
augusto-mattos <guto.s.mattos@gmail.com>
2022-10-24 16:07:42 +0200

    navigation bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// esse primeiro import foi acrescentado manualmente, nao foi criado automaticamente via create-react-app 
+import React from 'react';
+import Navbar from './components/Navbar';
+import "./App.scss";
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
     </div>
-  );
-}
+);
 
 export default App;
