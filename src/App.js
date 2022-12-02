9a255f1e2d892e93327669b487c27304936e640c
augusto-mattos <guto.s.mattos@gmail.com>
2022-12-02 15:24:40 +0100

    account history

--- src/App.js
+++ src/App.js
@@ -14,23 +14,35 @@ import Login from './views/Login';
 import Dashboard from './views/Dashboard';
 import AccountBalance from './components/AccountBalance';
 import AccountPayments from './components/AccountPayments';
+import AccountHistory from './components/AccountHistory';
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
 
   const data = {
     latestBalance: [
-    {date: '22/07', description: 'Supermercado 4516', value: '252,78'},
-    {date: '21/07', description: 'Saque 24h', value: '300,00'},
-    {date: '21/07', description: 'Estacionamento', value: '13,00'},
-    {date: '20/07', description: 'Aluguel', value: '1250.00,00'},
-    {date: '18/07', description: 'BarBar', value: '67,40'},
+      {date: '22/07', description: 'Supermercado 4516', value: '252,78'},
+      {date: '21/07', description: 'Saque 24h', value: '300,00'},
+      {date: '21/07', description: 'Estacionamento', value: '13,00'},
+      {date: '20/07', description: 'Aluguel', value: '1250.00,00'},
+      {date: '18/07', description: 'BarBar', value: '67,40'},
     ],
-  futureBalance: [
-    {date: '25/07', description: 'Spotify', value: '30,00'},
-    {date: '30/07', description: 'Netflix', value: '19,90'},
-    {date: '05/08', description: 'Gym', value: '59,00'},
-    {date: '05/08', description: 'Decathlon 2/4', value: '123,25'},
+    futureBalance: [
+      {date: '25/07', description: 'Spotify', value: '30,00'},
+      {date: '30/07', description: 'Netflix', value: '19,90'},
+      {date: '05/08', description: 'Gym', value: '59,00'},
+      {date: '05/08', description: 'Decathlon 2/4', value: '123,25'},
+    ],
+    history: [
+      {date: '17/07', description: 'S24h CHU', value:'200,00-'},
+      {date: '17/07', description: 'Saldo do dia', balance:'2780,00'},
+      {date: '18/07', description: 'Bar do João', value:'58,00-'},
+      {date: '18/07', description: 'Restô donté', value:'125,00-'},
+      {date: '18/07', description: 'Saldo do dia', balance:'2597,00'},
+      {date: '20/07', description: 'Vélo', value:'30,00-'},
+      {date: '20/07', description: 'Saldo do dia', balance:'2567,00'},
+      {date: '21/07', description: 'Café', value:'1,40-'},
+      {date: '21/07', description: 'Saldo do dia', balance:'2596,60'},
     ],
   };
 
@@ -44,7 +56,7 @@ const App = () => {
         <Route path='/dashboard' element={<Dashboard />}>
           <Route path='dashboard/balance' element={<AccountBalance data={data} />} />
           <Route path='/dashboard/payments' element={<AccountPayments />} />
-          <Route path='/dashboard/history' element={<h2>Extrato</h2>} />
+          <Route path='/dashboard/history' element={<AccountHistory data={data} />} />
         </Route>
       </Routes>
 
