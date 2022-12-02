4a4e7694e35148103e8c37ecdffd468f0eb77310
augusto-mattos <guto.s.mattos@gmail.com>
2022-12-02 14:31:42 +0100

    account payments

--- src/App.js
+++ src/App.js
@@ -12,21 +12,40 @@ import './App.scss';
 import Home from './views/Home';
 import Login from './views/Login';
 import Dashboard from './views/Dashboard';
+import AccountBalance from './components/AccountBalance';
+import AccountPayments from './components/AccountPayments';
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
+
+  const data = {
+    latestBalance: [
+    {date: '22/07', description: 'Supermercado 4516', value: '252,78'},
+    {date: '21/07', description: 'Saque 24h', value: '300,00'},
+    {date: '21/07', description: 'Estacionamento', value: '13,00'},
+    {date: '20/07', description: 'Aluguel', value: '1250.00,00'},
+    {date: '18/07', description: 'BarBar', value: '67,40'},
+    ],
+  futureBalance: [
+    {date: '25/07', description: 'Spotify', value: '30,00'},
+    {date: '30/07', description: 'Netflix', value: '19,90'},
+    {date: '05/08', description: 'Gym', value: '59,00'},
+    {date: '05/08', description: 'Decathlon 2/4', value: '123,25'},
+    ],
+  };
 
   return ( 
     <Router>
       <Navbar handleCreateAccount={() => setShowModal(true)} />
 
       <Routes> 
-        <Route path='/' element={<Home handleClick={() => setShowModal(true)} />} 
-        />
-        <Route path='/login' element={<Login />} 
-        />
-        <Route path='/dashboard' element={<Dashboard />} 
-        />
+        <Route path='/' element={<Home handleClick={() => setShowModal(true)} />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/dashboard' element={<Dashboard />}>
+          <Route path='dashboard/balance' element={<AccountBalance data={data} />} />
+          <Route path='/dashboard/payments' element={<AccountPayments />} />
+          <Route path='/dashboard/history' element={<h2>Extrato</h2>} />
+        </Route>
       </Routes>
 
       <Footer />
